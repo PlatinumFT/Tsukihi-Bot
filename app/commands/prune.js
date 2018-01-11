e99c27377442b21563a5ac925689013253e1281f
PlatinumFT <platinum2183@gmail.com>
2018-01-11 20:38:04 +0000

    test prune command

---
+++ app/commands/prune.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args) => {
+    if (args[0]) {
+        if(isNan(args[0])) return message.channel.send(`Please use a valid number!`);
+
+        const fetched = await message.channel.fetchMessages({limit: args[0]});
+        console.log(fetched);
+    } else {
+
+    }
+}
+
+exports.help = {
+    name: "prune",
+    description: "Prunes messages. No message will by default prune the bot.",
+    usage: "prune",
+    type: "moderation"    
+}
