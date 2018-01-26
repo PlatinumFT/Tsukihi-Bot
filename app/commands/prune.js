0e9e6bda569915f572a88df04fdc74f998dc0d72
PlatinumfT <platinum2183@gmail.com>
2018-01-26 11:30:31 +0000

    fixed prune

--- app/commands/prune.js
+++ app/commands/prune.js
@@ -1,12 +1,11 @@
 exports.run = async (client, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.sendMessage("You don't have the correct permissions to prune!");
     if (args[0]) {
-        if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.sendMessage("You don't have the correct permissions to prune!");
-        if(!parseInt(args[0])) return message.channel.send(`Please use a valid number!`);
-
         const fetched = await message.channel.fetchMessages({limit: args[0]});
         message.channel.bulkDelete(fetched)
             .catch(error => message.channel.send(`Error: ${error}`));
     } else {
+        if(!parseInt(args[0])) return message.channel.send(`Please use a valid number!`);
         const fetched = await message.channel.fetchMessages({limit: 40});
         let botMessages = [];
         fetched.forEach(e => {
--- app/events/ready.js
+++ app/events/ready.js
@@ -1,12 +1,11 @@
 var colors = require('colors');
 var os = require('os');
 module.exports = client => {
+	let settings=client.settings
     console.log(`${client.user.username} has started`.bold.magenta)
-    console.log(`|----------------------|`);
     console.log(` Users: `.bold.red + `${client.users.size}`)
     console.log(` Channels: `.bold.red + `${client.channels.size}`)
     console.log(` Guilds: `.bold.red + `${client.guilds.size}`)
-    console.log(`|----------------------|`);
 
     require('../util/commandsLoader.js')(client);    
 }
