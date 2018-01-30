aae152435ce8ed827acbd562ae7b294d10347838
PlatinumFT <platinum2183@gmail.com>
2018-01-30 23:58:15 +0000

    osutop command

---
+++ app/commands/osutop.js
@@ -0,0 +1,47 @@
+const Discord = module.require("discord.js");
+const Nodesu = require("nodesu");
+
+module.exports.run = async (client, message, args) => {
+    let res = await client.db(`SELECT osu_key FROM SETTINGS`);
+    if(!res[0]) return message.channel.send(`There is no osu api key set!`);
+    let apiKey = res[0];
+    const osu = await new Nodesu.Client(apiKey);
+
+    let user = await osu.user.get(args[0]);
+    let g = await osu.user.getBest(args[0], 0, 5);
+    str="";
+
+    for(i=0;i<g.length;i++) {
+        let play = g[i]
+        b = await osu.beatmaps.getByBeatmapId(play.beatmap_id);
+
+        var num300 = parseInt(play.count300),
+        numGeki = parseInt(play.countgeki),
+        num100 = parseInt(play.count100),
+        numKatu = parseInt(play.countkatu),
+        num50 = parseInt(play.count50), 
+        numMiss = parseInt(play.countmiss);
+
+        var topRow = ((50 * num50) + (100 * num100) + (300 * num300));
+        var bottomRow = (3 * (numMiss + num50 + num100 + num300));
+        accuracy =  Math.round(((topRow / bottomRow) * 100)) / 100;
+
+        str+=`**${i+1}.** ${b[0].title}  https://osu.ppy.sh/b/${play.beatmap_id} \n**Rank:** ${play.rank} | **PP:** ${play.pp}\n**Score:** ${play.score} | **Combo:** ${play.maxcombo}\n**Acc:** ${accuracy} | **Stars:** ${parseFloat(b[0].difficultyrating).toFixed(2)}★\n\n`
+    }
+
+    let embed = new Discord.RichEmbed()
+                            .setAuthor(`Top plays for ${user.username}`)
+                            .setDescription(str)
+                            .setThumbnail(`https://a.ppy.sh/${user.user_id}`)
+                            .setColor('FFFFFF');
+
+
+    message.channel.send(embed);
+}
+
+module.exports.help = {
+    name: "osutop",
+    description: "Checks a users top 5 plays.",
+    usage: "osutop platinumft",
+    type: "games"
+}
