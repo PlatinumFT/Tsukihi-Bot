23f10a0ca41fb0ac15b0214d7384a7280be76b01
PlatinumFT <platinum2183@gmail.com>
2018-02-05 22:53:11 +0000

    dm owner

--- app/events/message.js
+++ app/events/message.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 
 module.exports = async message => {
 
@@ -13,12 +14,13 @@ module.exports = async message => {
     let command = messageArray[0];
     let isOwner = false;
     if(settings.owner_id == message.author.id) isOwner = true;
+    
+    if(message.channel.type == 'dm') return dmOwner(message);
+    
     if(!command.startsWith(prefix)) {
         var res = await query(`SELECT * FROM guilds where guild_id='${message.guild.id}'`);
         if(res[0]) prefix = res[0].prefix;
     }
-    if(message.channel.type == 'dm') return;
-
     require('../util/xpHandler.js')(message);
 
     if(!command.startsWith(prefix)) return;
@@ -28,7 +30,22 @@ module.exports = async message => {
 
     if(!cmd) return;
 
-    if ((cmd.help.type == "owner" && !isOwner)) return;
+    if ((cmd.help.type == "owner" && !isOwner)) return console.log(':)');
 
     cmd.run(client, message, args);
 };
+
+function dmOwner(message) { 
+    const client = message.client;
+    
+    let c = client.users.get(client.settings.owner_id);
+
+    let embed = new Discord.RichEmbed()
+        .setAuthor(`Message recieved from ${message.author.username}`, message.author.displayAvatarURL)
+        .setColor('#FFFFFF')
+        .setDescription(`Content: ${message.content}`)
+        .setTimestamp()
+        .setFooter(`${message.author.id}`);
+
+    return c.send(embed);
+}
