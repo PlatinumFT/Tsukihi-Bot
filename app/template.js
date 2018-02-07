93fa7b9c26059980248edca6a5166734d69d7dc7
platinumFT <platinum2183@gmail.com>
2018-02-07 16:08:13 +0000

    added template, command to alter globalxp

---
+++ app/template.js
@@ -0,0 +1,15 @@
+module.exports.run = async (client, message, args) => {
+    // template for commands
+    // Insert async code here
+}
+
+module.exports.help = {
+    name: "",
+    description: "",
+    usage: "",
+    type: ""
+}
+
+module.exports.conf = {
+    aliases = []
+}
--- start.js
+++ start.js
@@ -1,14 +1,22 @@
 const bot = require('./app/bot.js');
 const query = require('./app/pgsql.js');
 const readlineSync = require('readline-sync');
+const colors = require('colors');
 
 run();
 
 async function run() {
+    await query("create table if not exists settings(token varchar, prefix varchar(10))");
     let res = await query("select * from settings");
-    if(!res[0]) return console.log('There is nothing here!');
+    if(!res[0]) setup();
+    else bot.run();
+}
 
-    bot.run();
+async function setup() {
+    console.log(`Welcome to Platinum's Bot!`.bold.yellow + `\nTo get started using this bot, you will need to supply:`.bold.yellow + `\n - Your token\n - Your ID\n - A global prefix\n`.bold.blue);
+    var token = readlineSync.question('Please paste your token here: ');
+    var id = readlineSync.question('Please enter your ID: ');
+    var prefix = readlineSync.question('Please select your prefix here: ');
 }
 /*functions = ['Start bot', 'Run migration'],
 index = readlineSync.keyInSelect(functions, 'What would you like to run?');
