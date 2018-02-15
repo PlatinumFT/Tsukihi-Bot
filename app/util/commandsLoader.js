56e04a993723588f05e1e7b717e860b92b393bc7
platinumFT <platinum2183@gmail.com>
2018-02-15 11:45:29 +0000

    commands are now stored in folders

--- app/util/commandsLoader.js
+++ app/util/commandsLoader.js
@@ -5,23 +5,29 @@ var colors = require('colors');
 module.exports = client => {
     client.commands = new Discord.Collection();
     client.aliases = new Discord.Collection();
-    fs.readdir("./app/commands/", (err, files) => {
+    fs.readdir("./app/commands", (err, files) => {
         if(err) console.error(err);
-        let jsfiles = files.filter(f => f.split(".").pop() === "js");
-        if(jsfiles.length <= 0 ) {
-            console.log("No commands to load!".red);
-            return;
-        }
-
-        console.log(`Loading ${jsfiles.length} commands!`.bold.magenta);
-            
-        jsfiles.forEach((f, i) => {
-            let props = require(`../commands/${f}`);
-            client.commands.set(props.help.name, props);
-            if(!props.conf || !props.conf.aliases) return;
-            props.conf.aliases.forEach(alias => {
-                client.aliases.set(alias, props.help.name);
+        files.forEach((f, i) => {
+            let folder = f.split('.');
+            if(folder[1]) return;
+            fs.readdir(`./app/commands/${f}/`, (err, jsf) => {
+                let jsfiles = jsf.filter(f => f.split(".").pop() === "js");
+                if(jsfiles.length <= 0 ) {
+                    console.log(`No commands found in ${f}`.red);
+                    return;
+                }
+    
+                console.log(`Loading ${jsfiles.length} commands in ${f}!`.bold.yellow)
+    
+                jsfiles.forEach((f, i) => {
+                    let props = require(`./app/commands/${folder}/${f}`);
+                    client.commands.set(props.help.name, props);
+                    if(!props.conf || !props.conf.aliases || props.conf.aliases[0] == '') return;
+                    props.conf.aliases.forEach(alias => {
+                        client.aliases.set(alias, props.help.name);
+                })
+            });
             })
-        });
+        })
     });
 }
