430a83805eb28379b8dee1dcde342f000b7fd805
PlatinumFT <platinum2183@gmail.com>
2018-03-03 18:57:21 +0000

    removed console logging

--- app/util/filter.js
+++ app/util/filter.js
@@ -9,11 +9,9 @@ module.exports = async (message, text) => {
     botRole = message.guild.members.get(client.user.id).highestRole;
 
     if (await botRole.comparePositionTo(myRole) <= 0) return;
-    
+
     let res = await query(`select * from filter where guild_id = '${message.guild.id}'`);
     if(!res[0]) return;
-    console.log(res);
-    console.log(text);
     res.forEach(e => {
         if(text.includes(e.phrase)) {
             message.delete();
