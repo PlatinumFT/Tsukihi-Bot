d505329afe88d6ca7b0e191cabf965af18e4b256
PlatinumFT <platinum2183@gmail.com>
2018-03-10 16:23:08 +0000

    filtering on message update

--- app/util/eventLoader.js
+++ app/util/eventLoader.js
@@ -9,6 +9,7 @@ module.exports = client => {
     client.on('disconnected', () => reqEvent('ready')(client));
     // client.on('guildMemberAdd', (member) => reqEvent('guildMemberAdd')(member,client));
     client.on('message', (message) => reqEvent('message')(message));
+    client.on('messageUpdate', (oldMessage, newMessage) => reqEvent('messageUpdate')(oldMessage, newMessage));
     // client.on('guildCreate', (guild) => reqEvent('guildCreate')(guild,client));
     // client.on('guildDelete', (guild) => reqEvent('guildDelete')(guild,client));
 }
