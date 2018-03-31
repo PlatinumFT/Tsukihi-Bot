2c8f4afc87b70a40bb4a9fa19e55ec23d1ad803a
PlatinumFT <platinum2183@gmail.com>
2018-03-31 17:44:42 +0100

    fixed userinfo

--- app/commands/util/userinfo.js
+++ app/commands/util/userinfo.js
@@ -7,30 +7,33 @@ module.exports.run = async (client, message, args) => {
 
     target = await client.findUser(message, text);
     if(!target) return message.channel.send("No user found!");
+    
+    let roles = "";
+    let count = 0;        
+    let guildMem = message.guild.members.get(target.id);
+    if(!guildMem) return message.channel.send(`User is not in this guild.`);
+    if(!guildMem.nickname) nickname = "None"
+    else nickname = guildMem.nickname;
 
-        let roles = "";
-        let count = 0;        
-        let guildMem = message.guild.members.get(target.id);
-        if(!guildMem) return message.channel.send(`User is not in this guild.`);
-        if(!guildMem.nickname) nickname = "None"
-        else nickname = guildMem.nickname;
-        
-        let embed = new Discord.RichEmbed()
-            .setAuthor(`User info for ${target.username}`, target.displayAvatarURL)
-            .setDescription(`Playing **${guildMem.presence.game.name}**`)
-            .setColor(await client.findColour(message, target))
-            .setThumbnail(target.displayAvatarURL)
-            .addField("Full Username", `${target.username}#${target.discriminator}`, true)
-            .addField("ID", `${target.id}`, true)
-            .addField("Nickname", `${nickname}`, true)
-            .addField("Status", `${guildMem.presence.status}`, true)            
-            .addField("Created At", moment(target.createdAt).format('MMMM Do YYYY, h:mm:ssa'), true)
-            .addField(`Roles - ${guildMem.roles.size}`, guildMem.roles.map(m => m.name).join(', '))
-            .setTimestamp();
+    let embed = new Discord.RichEmbed()
+        .setAuthor(`User info for ${target.username}`, target.displayAvatarURL)
+        .setColor(await client.findColour(message, target))
+        .setThumbnail(target.displayAvatarURL)
+        .addField("Full Username", `${target.username}#${target.discriminator}`, true)
+        .addField("ID", `${target.id}`, true)
+        .addField("Nickname", `${nickname}`, true)
+        .addField("Status", `${guildMem.presence.status}`, true)            
+        .addField("Created At", moment(target.createdAt).format('MMMM Do YYYY, h:mm:ssa'))
+        .addField("Joined", moment(guildMem.joinedAt).format('MMMM Do YYYY, h:mm:ssa'))
+        .addField(`Roles - ${guildMem.roles.size}`, guildMem.roles.map(m => m.name).join(', '))
+        .setTimestamp();
 
-        message.channel.send(embed);
+    if(target.presence.game)
+        embed.setDescription(`Playing **${target.presence.game.name}**`)
 
-        return;
+    message.channel.send(embed);
+
+    return;
 
 }
 
