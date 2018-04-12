5624a7e07b3da99990994a32d0f21a095bd58bac
PlatinumFT <platiiinum@gmail.com>
2018-04-12 12:41:40 +0100

    added limit role command

---
+++ app/commands/moderation/limitroles.js
@@ -0,0 +1,24 @@
+exports.run = async (client, message, args) => {
+    const query = client.db;
+    var res = await query(`SELECT * FROM guilds where guild_id='${message.guild.id}'`);
+    if(!args[1]) return message.channel.send('Please choose the amount of roles you want to limit!');
+
+    if(res[0]) query( `UPDATE guilds SET role_limit = '${args[1]}' WHERE guild_id = '${message.guild.id}'`);
+    else query(`INSERT INTO guilds(guild_id, role_limit) values ('${message.guild.id}', '${args[1]}')`);
+    return message.channel.send(`Role limit updated to ${args[1]}`);
+}
+
+
+exports.help = {
+    name: "limitroles",
+    description: "Limits the amount of limited roles a member can get.",
+    usage: "limitroles 2",
+    type: "help"
+}
+
+exports.conf = {
+    permissions: 
+    [
+        'MANAGE_ROLES',
+    ]
+}
