fd02cf2340ca809da25d78cf7246a7326c305f52
Jack Rogers <jack.rogers@nakedelement.co.uk>
2018-04-16 16:20:15 +0100

    discord authentication

--- app/bot.js
+++ app/bot.js
@@ -1,6 +1,11 @@
 const Discord = require("discord.js");
 const express = require('express');
 var path = require('path');
+var passport = require('passport');
+session  = require('express-session')
+var DiscordStrategy = require('passport-discord').Strategy
+var refresh = require('passport-oauth2-refresh');
+var scopes = ['identify', 'email', /* 'connections', (it is currently broken) */ 'guilds', 'guilds.join'];
 
 const client = new Discord.Client({disableEveryone: true});
       client.db = require('./pgsql.js');
@@ -36,16 +41,62 @@ funcs = {
         app.use(express.static(path.join(__dirname, 'webapp/public')));
         app.use(express.static(path.join(__dirname, '../bower_components')));
 
+        var discordStrat = new DiscordStrategy({
+            clientID: '396425352072921088',
+            clientSecret: 'Ph-bNVv-WHf7qF9aW4aq7B9zun9IF_Br',
+            callbackURL: 'http://localhost:3000/callback'
+        }, function(accessToken, refreshToken, profile, cb) {
+            profile.refreshToken = refreshToken; // store this for later refreshes
+            User.findOrCreate({ discordId: profile.id }, function(err, user) {
+                if (err)
+                    return done(err);
+         
+                return cb(err, user);
+            });
+        });
+
+        passport.use(new DiscordStrategy({
+            clientID: '396425352072921088',
+            clientSecret: 'Ph-bNVv-WHf7qF9aW4aq7B9zun9IF_Br',
+            callbackURL: 'http://localhost:3000/callback',
+            scope: scopes
+        }, function(accessToken, refreshToken, profile, done) {
+            process.nextTick(function() {
+                return done(null, profile);
+            });
+        }));
+        refresh.use(discordStrat);
+        app.use(session({
+            secret: 'keyboard cat',
+            resave: true,
+            saveUninitialized: true
+        }));
+        app.use(passport.initialize());
+        app.use(passport.session());
+
         app.set('views', path.join(__dirname, 'webapp/views'));
         app.set('view engine', 'ejs');
 
-        app.get('/user_roles/:id', async function(req, res) {
+        app.get('/login', passport.authenticate('discord', { scope: scopes }), function(req, res) {});
+        app.get('/callback',
+            passport.authenticate('discord', { failureRedirect: '/login' }), function(req, res) { res.redirect('/info') } // auth success
+        );
+        app.get('/logout', function(req, res) {
+            req.logout();
+            res.redirect('/login');
+        });
+        app.get('/info', checkAuth, function(req, res) {
+            //console.log(req.user)
+            res.json(req.user);
+        });
+
+        app.get('/user_roles/:id', checkAuth, async function(req, res) {
             let id = req.params.id;
             let result = await client.db(`select * from user_roles where user_id = '${id}'`);
             res.send(result);
         });
 
-        app.get('/guild/:id', async function(req, res) {
+        app.get('/guild/:id', checkAuth, async function(req, res) {
             let id = req.params.id;
             let result = await client.guilds.get(id);
             let r = {
@@ -57,7 +108,7 @@ funcs = {
             else res.send(null);
         })
 
-        app.get('/role/:guild_id/:role_id', async function(req, res) {
+        app.get('/role/:guild_id/:role_id', checkAuth, async function(req, res) {
             let guild_id = req.params.guild_id;
             let role_id = req.params.role_id;
 
@@ -74,7 +125,7 @@ funcs = {
             else res.send(null);
         })
 
-        app.get('/command_log', async function(req, res) {
+        app.get('/command_log', checkAuth, async function(req, res) {
             let result = await client.db(`select * from command_log`);
             let d = [];
             for(i=0; i<result.length;i++) {
@@ -113,3 +164,16 @@ funcs = {
         await query(`ALTER TABLE settings ADD COLUMN owner_id varchar(30)`);
     }
 }
+
+
+function checkAuth(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.send('not logged in :(');
+}
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
